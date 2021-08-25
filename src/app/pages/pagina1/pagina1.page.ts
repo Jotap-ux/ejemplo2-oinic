5967236d07e66dd74531c893282cc311fc0184a3
Jotap-ux <juanpablochato123@gmail.com>
2021-08-24 22:12:48 -0400

    confirmo2

--- src/app/pages/pagina1/pagina1.page.ts
+++ src/app/pages/pagina1/pagina1.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-pagina1',
   templateUrl: './pagina1.page.html',
   styleUrls: ['./pagina1.page.scss'],
 })
 export class Pagina1Page implements OnInit {
+  
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
 }
--- src/app/pages/pagina2/pagina2.page.ts
+++ src/app/pages/pagina2/pagina2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina2',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina2.page.scss'],
 })
 export class Pagina2Page implements OnInit {
+ 
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  
+  irhome(){
+    this.router.navigate(['/home']);
   }
 
 }
